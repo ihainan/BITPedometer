44906fc8bddd069b603b23a245f465ee10e23074
baichenchen <642858167@qq.com>
2015-01-12 20:12:41 +0800

    Revert "Revert "添加两个自己的包_test/byHK""
    This reverts commit 6b2a59ff6b25825c24988966edb7ac7dc59767f9.

---
+++ src/com/linc/pedometer/global/Global.java
@@ -0,0 +1,15 @@
+package com.linc.pedometer.global;
+
+import com.claire.pedometer.HomeActivity;
+
+public class Global {
+	
+	public static int stepValue;
+	public static int PaceValue;
+	public static float DistanceValue;
+	public static float SpeedValue;
+	public static int CaloriesValue;
+	
+	public static HomeActivity chartHomeActivity;
+
+}
